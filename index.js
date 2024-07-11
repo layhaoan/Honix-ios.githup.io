99dc6ed17415bed5b944459a134807d922ca5c78
layhaoan <nb2781049@gmail.com>
2024-07-11 10:19:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function openhead() {
+    document.getElementById("header").style.display = "block";
+  }
+  function closehead() {
+    document.getElementById("header").style.display = "none";
+  }
+
